05321433b948fe95ca924e08f778d6a4cc7ebcb2
Davi Maximo Quoos <davimaximoquooss@gmail.com>
2026-08-04 12:06:45 -0300

    feat(module-01): add API controller routing and query parameters

---
+++ 1-PrimeirosPassosASP.NET/4-Annotation[ApiController],[Route],[FromQuery]/WebApplication1/WebApplication1/Controllers/MathController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum);
+            }
+            return BadRequest("Invalid input.");
+        }
+
+        private int ConvertToDecimal(string secondNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private decimal IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
